ca709622c1309a7dad0e67d753a0c1fb653afb46
Jason Peak <jpeak5@lsu.edu>
2015-03-11 09:10:19 -0500

    refactor to use OOP

--- convert.php
+++ convert.php
@@ -1,150 +1,160 @@
 <?php
 
-// Print a usage message
-function printUsage($argv){
-    $space = function($i){
-        $spc = "";
-        foreach(range(0,$i) as $notUsed){
-            $spc .= " ";
-        }
-        return $spc;
-    };
+class Converter {
+    // Print a usage message
+    function printUsage($argv){
+        $space = function($i){
+            $spc = "";
+            foreach(range(0,$i) as $notUsed){
+                $spc .= " ";
+            }
+            return $spc;
+        };
 
-    $buf = function($str) use($space){
-        return $space(strlen($space(10)) - strlen($str));
-    };
+        $buf = function($str) use($space){
+            return $space(strlen($space(10)) - strlen($str));
+        };
 
-    $ind = $space(5);
+        $ind = $space(5);
 
-    $str =  sprintf("\nUsage: php %s src target width height quality \n\n", $argv[0]);
-    $str .= sprintf("%ssrc%sdirectory from which to fetch TIFFs for conversion\n", $ind, $buf("src"));
-    $str .= sprintf("%starget%sdirectory into which converted files will be saved\n", $ind, $buf('target'));
-    $str .= sprintf("%swidth%starget resize width; calculated if entered as 0; ignored if both dimensions are 0.\n", $ind, $buf('width'));
-    $str .= sprintf("%sheight%starget resize height; calculated if entered as 0; ignored if both dimensions are 0.\n", $ind, $buf('height'));
-    $str .= sprintf("%squality%starget compression quality 1-100.\n\n", $ind, $buf('quality'));
-    print $str;
-}
+        $str =  sprintf("\nUsage: php %s src target width height quality \n\n", $argv[0]);
+        $str .= sprintf("%ssrc%sdirectory from which to fetch TIFFs for conversion\n", $ind, $buf("src"));
+        $str .= sprintf("%starget%sdirectory into which converted files will be saved\n", $ind, $buf('target'));
+        $str .= sprintf("%swidth%starget resize width; calculated if entered as 0; ignored if both dimensions are 0.\n", $ind, $buf('width'));
+        $str .= sprintf("%sheight%starget resize height; calculated if entered as 0; ignored if both dimensions are 0.\n", $ind, $buf('height'));
+        $str .= sprintf("%squality%starget compression quality 1-100.\n\n", $ind, $buf('quality'));
+        print $str;
+    }
 
-if(count($argv) === 1){
-    printUsage($argv);
-    exit(0);
-}
+    public $src, $target, $width, $height, $quality, $windows, $executable, $command;
+
+    public function __construct($argv){
+
+        // Source directory; don't include the trailing slash.
+        $this->src     = new SplFileInfo($argv[1]);
+
+        // Destination for converted files; exclude trailing slash.
+        $this->target  = new SplFileInfo($argv[2]);
+
+        // Resize width; 0 for calculated.
+        $this->width   = $argv[3] === "0" ? '' : $argv[3];
+
+        // Resize height; 0 for calculated.
+        $this->height  = $argv[4] === "0" ? '' : $argv[4];
+
+        // Quality (1-100);
+        $this->quality = $argv[5];
+
+        // Setup environment-dependent properties.
+        defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+        $this->windows    = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+        $this->executable = $this->windows ? 'converte.exe.' : 'convert';
+
+        // init command
+        $dimensions = $this->width == '' && $this->height == '' ? '' : sprintf("-resize %sx%s", $this->width, $this->height);
+        $this->command = sprintf("%s %%s[0] -depth 8 %s -quality %d -quiet %%s", $this->executable, $dimensions, $this->quality);
+    }
 
 
+    /**
+     * Main processing loop.
+     *
+     * @var SplFileInfo $src source directory of Tiffs for conversion.
+     * @var SplFileInfo $src Target directory for converted images.
+     */
+    function processDir(SplFileInfo $src, SplFileInfo $target){
+        printf("Processing directory %s\n", $src->getRealPath());
 
-// Source directory; don't include the trailing slash.
-$src     = new SplFileInfo($argv[1]);
+        foreach(scandir($src->getRealPath()) as $file){
+            $file = new SplFileInfo($src->getRealPath().DS.$file);
 
-// Destination for converted files;p exclude trailing slash.
-$target  = new SplFileInfo($argv[2]);
+            // Do not try to process '.' and '..' directories.
+            if($file->getFileName() === '.' || $file->getFileName() === '..'){
+                printf("Skipping dot file '%s'\n", $file->getFilename());
+                continue;
+            }
 
-// Resize width; 0 for calculated.
-$width   = $argv[3] === "0" ? '' : $argv[3];
+            // Recurse into subdirectories.
+            if($file->isDir()){
+                $subtarget = new SplFileInfo($target->getRealPath().DS.$file->getFilename());
 
-// Resize height; 0 for calculated.
-$height  = $argv[4] === "0" ? '' : $argv[4];
+                // Create target subdirectory.
+                mkdir($subtarget->getRealPath());
 
-// Quality (1-100);
-$quality = $argv[5];
+                // Recursive call.
+                $this->processDir($file, $subtarget);
+                continue;
+            }
 
-// Setup environment-dependent properties.
-defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-$windows    = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
-$executable = $windows ? 'converte.exe.' : 'convert';
-
-
-
-/**
- * Main processing loop.
- *
- * @var SplFileInfo $src source directory of Tiffs for conversion.
- * @var SplFileInfo $src Target directory for converted images.
- */
-function processDir(SplFileInfo $src, SplFileInfo $target, $command){
-    printf("Processing directory %s\n", $src->getRealPath());
-
-    foreach(scandir($src->getRealPath()) as $file){
-        $file = new SplFileInfo($src->getRealPath().DS.$file);
-
-        // Do not try to process '.' and '..' directories.
-        if($file->getFileName() === '.' || $file->getFileName() === '..'){
-            printf("Skipping dot file '%s'\n", $file->getFilename());
-            continue;
-        }
-
-        // Recurse into subdirectories.
-        if($file->isDir()){
-            $subtarget = new SplFileInfo($target->getRealPath().DS.$file->getFilename());
-
-            // Create target subdirectory.
-            mkdir($subtarget->getRealPath());
-
-            // Recursive call.
-            processDir($file, $subtarget, $command);
-            continue;
-        }
-
-        // Don't process anything other than tiffs.
-        // TODO let input format be user-defined.
-        if(substr($file->getExtension(), 0, 3) !== 'tif'){
-            printf("Extension %s\n", substr($file->getExtension(), 0, 3));
-            printf("Skipping non-TIFF file %s\n", $file->getFilename());
-            continue;
-        }
+            // Don't process anything other than tiffs.
+            // TODO let input format be user-defined.
+            if(substr($file->getExtension(), 0, 3) !== 'tif'){
+                printf("Extension %s\n", substr($file->getExtension(), 0, 3));
+                printf("Skipping non-TIFF file %s\n", $file->getFilename());
+                continue;
+            }
 
         
-        //$newPath = $target.'/'.baseFilename($file).'.jp2';
-        $newPath = sprintf("%s%s%s.jp2", $target->getRealPath(), DS, baseFilename($file));
+            //$newPath = $target.'/'.baseFilename($file).'.jp2';
+            $newPath = sprintf("%s%s%s.jp2", $target->getRealPath(), DS, $this->baseFilename($file));
 
-        // Print message and start timer.
-        printf("Processing %s\n", $file->getRealPath());
+            // Print message and start timer.
+            printf("Processing %s\n", $file->getRealPath());
 
-        $cmd = sprintf($command, escapePath($file->getRealPath()), escapePath($newPath));
-        printf("  %s\n", $cmd);
-        $start = microtime(true);
+            $cmd = sprintf($this->command, $this->escapePath($file->getRealPath()), $this->escapePath($newPath));
+            printf("  %s\n", $cmd);
+            $start = microtime(true);
 
-        // Execute imagemagick executable.
-        exec($cmd);
+            // Execute imagemagick executable.
+            exec($cmd);
 
-        // Stop timer and finish status message.
-        $lapsed = microtime(true) - $start;
-        printf("  compressed %f Mb to %f Mb in %f s\n\n", mb($file->getSize()), mb(filesize($newPath)), $lapsed);
+            // Stop timer and finish status message.
+            $lapsed = microtime(true) - $start;
+            printf("  compressed %f Mb to %f Mb in %f s\n\n", $this->mb($file->getSize()), $this->mb(filesize($newPath)), $lapsed);
+        }
+    }
+
+    /**
+     * Convert bytes to megabytes
+     * @var int $bytes number of bytes
+     * @return float megabytes
+     */
+    function mb($bytes){
+        return $bytes/1024/1024;
+    }
+
+    /**
+     * Remove the file extension from a filename.
+     * Depends on file extension being the last characters after the last period '.'.
+     * @var SplFileInfo $file a file
+     * @return string the base filename without the extension
+     */
+    function baseFilename(SplFileInfo $file){
+        $newFilenameParts = explode('.', $file->getFilename());
+        array_pop($newFilenameParts);
+        return implode($newFilenameParts);
+    }
+
+    /**
+     * For Unix, escape whitespace.
+     * @var string $str the input string
+     * @return input string with spaces escaped with '\'
+     */
+    function escapePath($str){
+        return preg_replace('/ /', '\ ', $str);
     }
 }
 
-/**
- * Convert bytes to megabytes
- * @var int $bytes number of bytes
- * @return float megabytes
- */
-function mb($bytes){
-    return $bytes/1024/1024;
+
+$converter = new Converter($argv, $command);
+
+if(count($argv) === 1){
+    $converter->printUsage($argv);
+    exit(0);
 }
 
-/**
- * Remove the file extension from a filename.
- * Depends on file extension being the last characters after the last period '.'.
- * @var SplFileInfo $file a file
- * @return string the base filename without the extension
- */
-function baseFilename(SplFileInfo $file){
-    $newFilenameParts = explode('.', $file->getFilename());
-    array_pop($newFilenameParts);
-    return implode($newFilenameParts);
-}
+//var_dump($converter);die();
 
-/**
- * For Unix, escape whitespace.
- * @var string $str the input string
- * @return input string with spaces escaped with '\'
- */
-function escapePath($str){
-    return preg_replace('/ /', '\ ', $str);
-};
-
-$dimensions = $width == '' && $height == '' ? '' : sprintf("-resize %sx%s", $width, $height);
-$command = sprintf("%s %%s[0] -depth 8 %s -quality %d -quiet %%s", $executable, $dimensions, $quality);
-
-processDir($src, $target, $command);
+$converter->processDir($converter->src, $converter->target);
 
 ?>
